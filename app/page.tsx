80a0d1f5beb39db23e7d48d93f4ee88edd8ff745
SyedEbad902 <ebad3088@gmail.com>
2024-10-10 04:57:34 +0500

    PW website clone

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,170 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="w-full h-full bg-white">
+      {/* Navbar starts here */}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <nav>
+        <div className=" shadow-md h-[70px] flex place-content-evenly w-full  p-4">
+          <div className="  flex min-w-300">
+            <img
+              className="w-13 h-10 pl-5"
+              src="https://i.pinimg.com/originals/8e/23/a2/8e23a255af007c366bc1c75ab4850942.png"
+              alt=""
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <p className="pl-1 pt-2 font-bold  text-1xl">SKILLS</p>
+          </div>
+
+          <div className="">
+            <select
+              className="w-36 h-10  bg-gray-100 rounded-sm "
+              name="cars"
+              id="cars"
+            >
+              <option value="volvo">Volvo</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>{" "}
+          </div>
+          <div className="  w-[400px] p-2 pr-10">
+            <ul className=" place-content-between flex">
+              <li className="font-semibold text-sm">Online Degree</li>
+              <li className="font-semibold text-sm">PW 101</li>
+              <li className="font-semibold text-sm">Masterclass</li>
+            </ul>
+          </div>
+
+          <div>
+            <input
+              className=" h-11 w-64 bg-gray-200 rounded-sm p-2"
+              type="search "
+              placeholder="Search Courses"
+            />
+          </div>
+          <div>
+            <button className="border-2  h-11 border-orange-400 rounded-lg w-36 ">
+              <p className="text-orange-400 font-bold ">Login/Register</p>
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      </nav>
+      {/* navbar ends here */}
+
+      {/* hero section starts here */}
+      <div className="w-full h-auto flex justify-between ">
+        <div className="pt-32 pl-20 w-1/2">
+          <p className="font-bold text-4xl mb-4">Stay Ahead Of The Curve</p>
+          <p className="font-bold text-4xl mb-4">With Our AFFORDALE </p>
+          <p className="font-bold text-4xl">Courses</p>
+          <p className=" mt-8 text-slate-600">
+            PW Skills is your one-stop-shop for upscaling. Get maximum value for
+            time <br />
+            and resources you invest, with job-ready courses & high-technology,{" "}
+            <br />
+            available at the lowest cost.
+          </p>
+          <button className="h-12 border-2 rounded-lg bg-orange-500 w-48 text-white mt-8">
+            Eplore Courses
+          </button>
+        </div>
+        <div className="m-2 w-1/2">
+          <img
+            className="bg-cover "
+            src="https://thumbs.dreamstime.com/b/education-word-cloud-19119234.jpg"
+            alt=""
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+        </div>
+      </div>
+      {/* hero section ends here */}
+
+      {/* card section starts here */}
+      <div className=" h-auto w-auto pt-10 flex justify-around px-14 ">
+        <div className="flex bg-slate-200 shadow-lg p-3 rounded-md w-64">
+          <div>
+            <img
+              src="https://pwskills.com/images/homePage/statistics1.png"
+              alt=""
+            />
+          </div>
+          <div className="ml-2">
+            <p className="font-bold text-lg">55%</p>
+            <p>Average Salary Hike</p>
+          </div>
+        </div>
+        <div className="flex bg-slate-200 shadow-lg p-3 rounded-md w-64">
+          <div>
+            <img
+              src="https://pwskills.com/images/homePage/statistics2.png"
+              alt=""
+            />
+          </div>
+          <div className="ml-2">
+            <p className="font-bold text-lg">55%</p>
+            <p>Average Salary Hike</p>
+          </div>
+        </div>
+        <div className="flex bg-slate-200 shadow-lg p-3 rounded-md w-64">
+          <div>
+            <img
+              src="https://pwskills.com/images/homePage/statistics3.png"
+              alt=""
+            />
+          </div>
+          <div className="ml-2">
+            <p className="font-bold text-lg">12000+</p>
+            <p>Career Transition</p>
+          </div>
+        </div>
+        <div className="flex bg-slate-200 shadow-lg p-3 rounded-md w-64">
+          <div>
+            <img
+              src="https://pwskills.com/images/homePage/statistics1.png"
+              alt=""
+            />
+          </div>
+          <div className="ml-2">
+            <p className="font-bold text-lg">55%</p>
+            <p>Average Salary Hike</p>
+          </div>
+        </div>
+      </div>
+      {/* card section end s here */}
+      <div className="h-auto w-auto bg-gray-50 mt-20 mb-20 flex ">
+        <div className="pt-32 pl-20 w-1/2">
+          <p className="font-bold text-lg mb-8">Online Degree</p>
+          <p className="font-bold text-6xl mb-4">Where Learning</p>
+          <p className="font-bold text-6xl mb-4">Meets The</p>
+          <p className="font-bold text-6xl mb-4">Expectation</p>
+          <p className="text-slate-600 font-bold text-lg mt-10">
+            At PW Skills, we are Revolutionising your <br />
+            journey of stepping your first foot into your <br />
+            Career through our college programs with <br />
+            partnering up with Top Tier Colleges
+          </p>
+          <button className="h-12 border-2 rounded-lg bg-orange-500 w-48 text-white mt-8">
+            Eplore Courses
+          </button>
+          <button className="h-12 border-2  rounded-lg border-orange-500 w-56 text-orange-500 mt-8 ml-5 ">
+            <p className="flex justify-center ">
+              {" "}
+              Talk to counsellor{" "}
+              <img
+                className="ml-3"
+                src="https://pwskills.com/_next/static/media/call.0887f2c7.svg"
+                alt=""
+              />
+            </p>
+          </button>
+        </div>
+
+        <div className="w-1/2 pt-20 pr-7"> 
+          <img className=" h-6/6 w-full"
+            src="https://pwskills.com/images/homePage/homepage-degree.webp"
+            alt=""
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
     </div>
   );
 }
